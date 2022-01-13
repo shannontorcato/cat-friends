c425cd057eb6168240f2ccb9c8aa1d4cb9cf0978
shannontorcato <shannon.torcato1234@gmail.com>
2022-01-13 16:48:15 -0500

    added background color and font style

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardList from "./CardList";
 import {robots} from './robots';
 import SearchBox from './SearchBox';
+import './App.css'
 
 class App extends Component {
     constructor(){
@@ -14,17 +15,16 @@ class App extends Component {
 
     onSearchChange = (event) => {
         this.setState({ searchfield: event.target.value})
-        
         }
 
     render(){
         const filteredCats = this.state.robots.filter(cat=>{
-            return cat.name.toLowerCase().includes(this.state.searchfield.toLocaleLowerCase())
+            return cat.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
         return (
             
             <div className="tc">
-                <h1>Cat Friends</h1>
+                <h1 className="f1">CAT FRIENDS</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <CardList robots={filteredCats}/>
             </div>
